f683865e2013eccb5f1bdd30c1df494be4aba416
Sharif_himu <sharif.himu@v2.ltd>
2021-12-13 18:06:45 +0600

    testing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,89 @@
+import React, { useState } from "react";
 import logo from './logo.svg';
 import './App.css';
+import Webcam from "react-webcam";
+
 
 function App() {
+
+  const [click, setClick] = useState(false)
+
+  const webcamRef = React.useRef(null);
+
+  const contentType = 'image/jpeg';
+
+  const b64toBlob = (b64Data, contentType='', sliceSize=512) => {
+    console.log('b64Data ', b64Data.substr(27, b64Data.length ) );
+    const byteCharacters = window.atob(b64Data.substr(27, b64Data.length ));
+    const byteArrays = [];
+    console.log('blob ', byteCharacters );
+    
+    for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+      const slice = byteCharacters.slice(offset, offset + sliceSize);
+  
+      const byteNumbers = new Array(slice.length);
+      for (let i = 0; i < slice.length; i++) {
+        byteNumbers[i] = slice.charCodeAt(i);
+      }
+  
+      const byteArray = new Uint8Array(byteNumbers);
+      byteArrays.push(byteArray);
+    }
+  
+    const blob = new Blob(byteArrays, {type: contentType});
+    return blob;
+  }
+
+  const capture = React.useCallback(
+    () => {
+      const imageSrc = webcamRef.current.getScreenshot();
+
+      console.log('img ', imageSrc );
+
+      const blob = b64toBlob(imageSrc, contentType);
+      const blobUrl = URL.createObjectURL(blob);
+
+      console.log('blob ', blobUrl );
+
+      var image = new Image();
+      image.src = imageSrc;
+      document.body.appendChild(image);
+
+      // console.log('url ', image.src );
+
+      setClick(false)
+    },
+    [webcamRef]
+  );
+
+  const videoConstraints = {
+    width: 720,
+    height: 720,
+    facingMode: "user"
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+    <button onClick={() => setClick(true)} > camera </button>
+
+    {
+      click &&
+      <>
+      <Webcam
+        audio={false}
+        height={400}
+        ref={webcamRef}
+        screenshotFormat="image/jpeg"
+        width={400}
+        videoConstraints={videoConstraints}
+      />
+      <button onClick={capture}>Capture photo</button>
+    </>
+    }
+
+    </>
   );
 }
 
